ab27fd65a6236f159190ced4007d65c467a92f56
pssncp142 <ydalliar@sabanciuniv.edu>
2012-12-27 17:01:52 +0200

    Thu Dec 27 17:01:52 EET 2012

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -25,14 +25,14 @@
 
 DetectorConstruction::DetectorConstruction()
 {
-	worldSize = 1*m;
-	detDistToMask = 34*mm;
-	maskPixSize  = 1.2*mm;
-	maskHeight = 2*mm;
-	calls = 0;
-	
-	detMess = new DetectorMessenger(this); 
- }
+  worldSize = 1*m;
+  detDistToMask = 34*mm;
+  maskPixSize  = 1.2*mm;
+  maskHeight = 2*mm;
+  calls = 0;
+  
+  detMess = new DetectorMessenger(this); 
+}
 
 DetectorConstruction::~DetectorConstruction()
 {
@@ -41,38 +41,38 @@ DetectorConstruction::~DetectorConstruction()
 
 G4VPhysicalVolume* DetectorConstruction::Construct()
 {
-	G4GeometryManager::GetInstance()->OpenGeometry();
+  G4GeometryManager::GetInstance()->OpenGeometry();
   G4PhysicalVolumeStore::GetInstance()->Clean();
   G4LogicalVolumeStore::GetInstance()->Clean();
   G4SolidStore::GetInstance()->Clean();
-
-	DefineMaterials();
-
+  
+  DefineMaterials();
+  
   World_sol = new G4Box("World",worldSize/2,worldSize/2,worldSize/2);		   
   World_log = new G4LogicalVolume(World_sol,Air,"World");		
   World_phys = new G4PVPlacement(0,G4ThreeVector(0,0,0),World_log,"World",0,false,0);  
   World_log->SetVisAttributes(G4VisAttributes::Invisible);
-
-	ConstructDetector();
+  
+  ConstructDetector();
   ConstructMask();
   
-	G4SDManager* SDman = G4SDManager::GetSDMpointer();
+  G4SDManager* SDman = G4SDManager::GetSDMpointer();
   sensDet = new SensitiveDetector("/SensDetector");
-	detect_log->SetSensitiveDetector(sensDet);	
+  detect_log->SetSensitiveDetector(sensDet);	
   SDman->AddNewDetector(sensDet);
-
+  
   return World_phys;
 } 
 
 void DetectorConstruction::ConstructDetector()
 {
-	detect_sol = new G4Box("detect_sol",19.54*0.5*mm,19.54*0.5*mm,5.*0.5*mm);
-	detect_log = new G4LogicalVolume(detect_sol,CdZnTe,"detect_log");
+  detect_sol = new G4Box("detect_sol",19.54*0.5*mm,19.54*0.5*mm,5.*0.5*mm);
+  detect_log = new G4LogicalVolume(detect_sol,CdZnTe,"detect_log");
   detect_log->SetVisAttributes(G4Color::Yellow());  
-
-	detect_phys = new G4PVPlacement(0,G4ThreeVector((19.54*0.5+1.2-0.34),(19.54*0.5+1.2-0.34),-detDistToMask)*mm,detect_log,"detect_phys",World_log,false,0);
+  
+  detect_phys = new G4PVPlacement(0,G4ThreeVector((19.54*0.5+1.2-0.34),(19.54*0.5+1.2-0.34),-detDistToMask)*mm,detect_log,"detect_phys",World_log,false,0);
   detect_phys = new G4PVPlacement(0,G4ThreeVector(-(19.54*0.5+1.2-0.34),-(19.54*0.5+1.2-0.34),-detDistToMask)*mm,detect_log,"detect_phys",World_log,false,0);
-	detect_phys = new G4PVPlacement(0,G4ThreeVector(-(19.54*0.5+1.2-0.34),(19.54*0.5+1.2-0.34),-detDistToMask)*mm,detect_log,"detect_phys",World_log,false,0);
+  detect_phys = new G4PVPlacement(0,G4ThreeVector(-(19.54*0.5+1.2-0.34),(19.54*0.5+1.2-0.34),-detDistToMask)*mm,detect_log,"detect_phys",World_log,false,0);
   detect_phys = new G4PVPlacement(0,G4ThreeVector((19.54*0.5+1.2-0.34),-(19.54*0.5+1.2-0.34),-detDistToMask)*mm,detect_log,"detect_phys",World_log,false,0);
 }
 
@@ -80,88 +80,88 @@ void DetectorConstruction::ConstructMask()
 {
   pixel_sol = new G4Box("pixel_sol",maskPixSize*0.5*mm,maskPixSize*0.5*mm,maskHeight*0.5*mm);
   strip_sol = new G4Box("strip_sol",(36+0.5)*maskPixSize*mm,maskPixSize*0.5*mm,maskHeight*mm);
-	pixel_log = new G4LogicalVolume(pixel_sol,Pb,"pixel_log");
-	strip_log = new G4LogicalVolume(strip_sol,Pb,"strip_log");
+  pixel_log = new G4LogicalVolume(pixel_sol,Pb,"pixel_log");
+  strip_log = new G4LogicalVolume(strip_sol,Pb,"strip_log");
   
   bool maskarr[36] = {1,0,1,1,0,0,1,0,1,1,1,1,0,0,0,1,0,0,0,0,1,0,0,0,1,1,1,1,0,1,0,0,1,1,0,1};
-	bool maskapf[36][36];
+  bool maskapf[36][36];
   for (int i=0;i<36;i++)
+    {
+      for (int j=0;j<36;j++)
 	{
-		for (int j=0;j<36;j++)
-		{
-      if (maskarr[i] == maskarr[j]) {maskapf[i][j]=1;} else {maskapf[i][j]=0;}
-		}
+	  if (maskarr[i] == maskarr[j]) {maskapf[i][j]=1;} else {maskapf[i][j]=0;}
 	}
-
+    }
+  
   int xpos[4] = {-36,1,-36,1};
-	int ypos[4] = {36,36,-1,-1};
-	for (int k=0;k<4;k++)
+  int ypos[4] = {36,36,-1,-1};
+  for (int k=0;k<4;k++)
+    {
+      for (int i=0;i<36;i++)
 	{
-	 	for (int i=0;i<36;i++)
+	  for (int j=0;j<36;j++)
+	    {
+	      if (maskapf[i][j]==1)
 		{
-			for (int j=0;j<36;j++)
-			{
- 		 		if (maskapf[i][j]==1)
-				{
-					mask_phys = new G4PVPlacement(0,G4ThreeVector(-(xpos[k]+i),(ypos[k]-j),0)*maskPixSize*mm,
-																					 pixel_log,"mask_phys",World_log,false,0);						
- 				}
-			}
+		  mask_phys = new G4PVPlacement(0,G4ThreeVector(-(xpos[k]+i),(ypos[k]-j),0)*maskPixSize*mm,
+						pixel_log,"mask_phys",World_log,false,0);						
 		}
+	    }
 	}
+    }
 
-	mask_phys = new G4PVPlacement(0,G4ThreeVector(0,0,0),strip_log,"mask_phys",World_log,false,0);
+  mask_phys = new G4PVPlacement(0,G4ThreeVector(0,0,0),strip_log,"mask_phys",World_log,false,0);
   mask_phys = new G4PVPlacement(0,G4ThreeVector(0,37,0)*maskPixSize*mm,strip_log,"mask_phys",World_log,false,0);
   mask_phys = new G4PVPlacement(0,G4ThreeVector(0,-37,0)*maskPixSize*mm,strip_log,"mask_phys",World_log,false,0);
-
-	rotm = new G4RotationMatrix();
-	rotm->rotateZ(90*degree);
-	mask_phys = new G4PVPlacement(rotm,G4ThreeVector(37,0,0)*maskPixSize*mm,strip_log,"mask_phys",World_log,false,0);
+  
+  rotm = new G4RotationMatrix();
+  rotm->rotateZ(90*degree);
+  mask_phys = new G4PVPlacement(rotm,G4ThreeVector(37,0,0)*maskPixSize*mm,strip_log,"mask_phys",World_log,false,0);
   mask_phys = new G4PVPlacement(rotm,G4ThreeVector(-37,0,0)*maskPixSize*mm,strip_log,"mask_phys",World_log,false,0);
-
-	mask_phys = new G4PVPlacement(0,G4ThreeVector(37,37,0)*maskPixSize*mm,pixel_log,"mask_phys",World_log,false,0);
+  
+  mask_phys = new G4PVPlacement(0,G4ThreeVector(37,37,0)*maskPixSize*mm,pixel_log,"mask_phys",World_log,false,0);
   mask_phys = new G4PVPlacement(0,G4ThreeVector(-37,-37,0)*maskPixSize*mm,pixel_log,"mask_phys",World_log,false,0);
-	mask_phys = new G4PVPlacement(0,G4ThreeVector(-37,37,0)*maskPixSize*mm,pixel_log,"mask_phys",World_log,false,0);
+  mask_phys = new G4PVPlacement(0,G4ThreeVector(-37,37,0)*maskPixSize*mm,pixel_log,"mask_phys",World_log,false,0);
   mask_phys = new G4PVPlacement(0,G4ThreeVector(37,-37,0)*maskPixSize*mm,pixel_log,"mask_phys",World_log,false,0);	
 }
 
 void DetectorConstruction::DefineMaterials()
 {
-	G4String symbol;
-	G4double a,density;
-	G4int z,natoms,ncomponents;
-	#define GET_MATERIAL G4NistManager::Instance()->FindOrBuildMaterial
+  G4String symbol;
+  G4double a,density;
+  G4int z,natoms,ncomponents;
+#define GET_MATERIAL G4NistManager::Instance()->FindOrBuildMaterial
   Air = GET_MATERIAL("G4_AIR"); 
   Pb = GET_MATERIAL("G4_Pb");
   Al = GET_MATERIAL("G4_Al");
-	G4Element* Cd = new G4Element("Cadmium", symbol="Cd", z=48, a=112.411*g/mole);
+  G4Element* Cd = new G4Element("Cadmium", symbol="Cd", z=48, a=112.411*g/mole);
   G4Element* Zn = new G4Element("Zinc", symbol="Zn", z=30, a=65.39*g/mole);
   G4Element* Te = new G4Element("Tellurium", symbol="Te", z=52, a=127.6*g/mole);
-	CdZnTe = new G4Material("CdZnTe",density=5.78*g/cm3,ncomponents=3);
+  CdZnTe = new G4Material("CdZnTe",density=5.78*g/cm3,ncomponents=3);
   CdZnTe->AddElement(Cd,natoms=9);
   CdZnTe->AddElement(Zn,natoms=1);
   CdZnTe->AddElement(Te,natoms=10);
-	#undef GET_MATERIAL
+#undef GET_MATERIAL
 }
 
 void DetectorConstruction::SetDetDistToMask(G4double val)
 {
-	detDistToMask = val;
+  detDistToMask = val;
 }
 
 void DetectorConstruction::SetMaskPixSize(G4double val)
 {
-	maskPixSize = val;
+  maskPixSize = val;
 }
 
 void DetectorConstruction::SetMaskHeight(G4double val)
 {
-	maskHeight = val;
+  maskHeight = val;
 }
 
 #include "G4RunManager.hh"
 
 void DetectorConstruction::UpdateGeometry()
 {
-	G4RunManager::GetRunManager()->DefineWorldVolume(Construct());
+  G4RunManager::GetRunManager()->DefineWorldVolume(Construct());
 }
---
+++ tmp/Linux-g++/mask_sim/SensitiveDetector.d
@@ -0,0 +1,246 @@
+/home/psyigit/Project/mask_sim/tmp/Linux-g++/mask_sim/SensitiveDetector.o /home/psyigit/Project/mask_sim/tmp/Linux-g++/mask_sim/SensitiveDetector.d: src/SensitiveDetector.cc \
+ include/SensitiveDetector.hh include/TrackHit.hh \
+ /usr/local/geant/include/Geant4/G4VHit.hh \
+ /usr/local/geant/include/Geant4/globals.hh \
+ /usr/local/geant/include/Geant4/G4ios.hh \
+ /usr/local/geant/include/Geant4/G4Types.hh \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/complex \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/c++config.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/os_defines.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-32.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/cpu_defines.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/cpp_type_traits.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/ext/type_traits.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/cmath \
+ /usr/include/math.h /usr/include/bits/huge_val.h \
+ /usr/include/bits/huge_valf.h /usr/include/bits/huge_vall.h \
+ /usr/include/bits/inf.h /usr/include/bits/nan.h \
+ /usr/include/bits/mathdef.h /usr/include/bits/mathcalls.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/cmath.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/sstream \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/istream \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/ios \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/iosfwd \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stringfwd.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/postypes.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/cwchar \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/cstddef \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/include/stddef.h \
+ /usr/include/wchar.h /usr/include/stdio.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/include/stdarg.h \
+ /usr/include/bits/wchar.h /usr/include/xlocale.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/exception \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/char_traits.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_algobase.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/functexcept.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/exception_defines.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/ext/numeric_traits.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_pair.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/move.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/concept_check.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_iterator_base_types.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_iterator_base_funcs.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_iterator.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/debug/debug.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/localefwd.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/c++locale.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/clocale \
+ /usr/include/locale.h /usr/include/bits/locale.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/cctype \
+ /usr/include/ctype.h /usr/include/bits/types.h \
+ /usr/include/bits/typesizes.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/ios_base.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/ext/atomicity.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/gthr.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h /usr/include/time.h \
+ /usr/include/bits/sched.h /usr/include/bits/time.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/setjmp.h \
+ /usr/include/unistd.h /usr/include/bits/posix_opt.h \
+ /usr/include/bits/environments.h /usr/include/bits/confname.h \
+ /usr/include/getopt.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/atomic_word.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/locale_classes.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/string \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/allocator.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/c++allocator.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/ext/new_allocator.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/new \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/ostream_insert.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/cxxabi-forced.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_function.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/backward/binders.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/basic_string.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/initializer_list \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/basic_string.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/locale_classes.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/streambuf \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/streambuf.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/basic_ios.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/locale_facets.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/cwctype \
+ /usr/include/wctype.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/ctype_base.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/streambuf_iterator.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/ctype_inline.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/locale_facets.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/basic_ios.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/ostream \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/ostream.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/istream.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/sstream.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/iostream \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/algorithm \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_algo.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/cstdlib \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/sys/types.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/sigset.h /usr/include/sys/sysmacros.h \
+ /usr/include/alloca.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/algorithmfwd.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_heap.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_tempbuf.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_construct.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_uninitialized.h \
+ /usr/local/geant/include/Geant4/G4String.hh /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/bits/stdio_lim.h \
+ /usr/include/bits/sys_errlist.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/cstring \
+ /usr/include/string.h /usr/local/geant/include/Geant4/G4String.icc \
+ /usr/local/geant/include/Geant4/templates.hh \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/limits \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/climits \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/include/limits.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /usr/local/geant/include/Geant4/G4PhysicalConstants.hh \
+ /usr/local/geant/include/Geant4/CLHEP/Units/PhysicalConstants.h \
+ /usr/local/geant/include/Geant4/CLHEP/Units/SystemOfUnits.h \
+ /usr/local/geant/include/Geant4/G4SystemOfUnits.hh \
+ /usr/local/geant/include/Geant4/G4ExceptionSeverity.hh \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/vector \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_vector.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_bvector.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/vector.tcc \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/map \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_tree.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_map.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/stl_multimap.h \
+ /usr/local/geant/include/Geant4/G4THitsCollection.hh \
+ /usr/local/geant/include/Geant4/G4VHitsCollection.hh \
+ /usr/local/geant/include/Geant4/G4Allocator.hh \
+ /usr/local/geant/include/Geant4/G4AllocatorPool.hh \
+ /usr/local/geant/include/Geant4/G4Material.hh \
+ /usr/local/geant/include/Geant4/G4Element.hh \
+ /usr/local/geant/include/Geant4/G4Isotope.hh \
+ /usr/local/geant/include/Geant4/G4IonisParamElm.hh \
+ /usr/local/geant/include/Geant4/G4IsotopeVector.hh \
+ /usr/local/geant/include/Geant4/G4ElementTable.hh \
+ /usr/local/geant/include/Geant4/G4MaterialPropertiesTable.hh \
+ /usr/local/geant/include/Geant4/G4MaterialPropertyVector.hh \
+ /usr/local/geant/include/Geant4/G4PhysicsOrderedFreeVector.hh \
+ /usr/local/geant/include/Geant4/G4PhysicsVector.hh \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/fstream \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/codecvt.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/cstdio \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/basic_file.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/i686-redhat-linux/bits/c++io.h \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/bits/fstream.tcc \
+ /usr/local/geant/include/Geant4/G4PhysicsVectorCache.hh \
+ /usr/local/geant/include/Geant4/G4PhysicsVectorType.hh \
+ /usr/local/geant/include/Geant4/G4PhysicsVector.icc \
+ /usr/local/geant/include/Geant4/G4PhysicsOrderedFreeVector.icc \
+ /usr/local/geant/include/Geant4/G4MaterialPropertiesTable.icc \
+ /usr/local/geant/include/Geant4/G4IonisParamMat.hh \
+ /usr/local/geant/include/Geant4/G4SandiaTable.hh \
+ /usr/local/geant/include/Geant4/G4OrderedTable.hh \
+ /usr/local/geant/include/Geant4/G4DataVector.hh \
+ /usr/local/geant/include/Geant4/G4DataVector.icc /usr/include/assert.h \
+ /usr/local/geant/include/Geant4/G4ElementVector.hh \
+ /usr/local/geant/include/Geant4/G4MaterialTable.hh \
+ /usr/local/geant/include/Geant4/G4Element.hh \
+ /usr/local/geant/include/Geant4/G4VPhysicalVolume.hh \
+ /usr/local/geant/include/Geant4/geomdefs.hh \
+ /usr/local/geant/include/Geant4/G4RotationMatrix.hh \
+ /usr/local/geant/include/Geant4/G4ThreeVector.hh \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/ThreeVector.h \
+ /usr/local/geant/include/Geant4/CLHEP/Utility/defs.h \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/ThreeVector.icc \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/Rotation.h \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/RotationInterfaces.h \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/LorentzVector.h \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/LorentzVector.icc \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/AxisAngle.h \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/AxisAngle.icc \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/RotationInterfaces.icc \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/RotationX.h \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/RotationX.icc \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/RotationY.h \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/RotationY.icc \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/RotationZ.h \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/RotationZ.icc \
+ /usr/local/geant/include/Geant4/CLHEP/Vector/Rotation.icc \
+ /usr/local/geant/include/Geant4/G4VPhysicalVolume.icc \
+ /usr/local/geant/include/Geant4/G4VSensitiveDetector.hh \
+ /usr/local/geant/include/Geant4/G4VHit.hh \
+ /usr/local/geant/include/Geant4/G4Step.hh \
+ /usr/lib/gcc/i686-redhat-linux/4.4.6/../../../../include/c++/4.4.6/iomanip \
+ /usr/local/geant/include/Geant4/G4VPhysicalVolume.hh \
+ /usr/local/geant/include/Geant4/G4StepPoint.hh \
+ /usr/local/geant/include/Geant4/G4SteppingControl.hh \
+ /usr/local/geant/include/Geant4/G4StepStatus.hh \
+ /usr/local/geant/include/Geant4/G4TouchableHandle.hh \
+ /usr/local/geant/include/Geant4/G4VTouchable.hh \
+ /usr/local/geant/include/Geant4/G4VTouchable.icc \
+ /usr/local/geant/include/Geant4/G4ReferenceCountedHandle.hh \
+ /usr/local/geant/include/Geant4/G4Material.hh \
+ /usr/local/geant/include/Geant4/G4LogicalVolume.hh \
+ /usr/local/geant/include/Geant4/G4Region.hh \
+ /usr/local/geant/include/Geant4/G4Region.icc \
+ /usr/local/geant/include/Geant4/G4LogicalVolume.icc \
+ /usr/local/geant/include/Geant4/G4StepPoint.icc \
+ /usr/local/geant/include/Geant4/G4TrackVector.hh \
+ /usr/local/geant/include/Geant4/G4Step.icc \
+ /usr/local/geant/include/Geant4/G4Track.hh \
+ /usr/local/geant/include/Geant4/G4DynamicParticle.hh \
+ /usr/local/geant/include/Geant4/G4ParticleDefinition.hh \
+ /usr/local/geant/include/Geant4/G4ParticleDefinition.icc \
+ /usr/local/geant/include/Geant4/G4LorentzVector.hh \
+ /usr/local/geant/include/Geant4/G4ParticleMomentum.hh \
+ /usr/local/geant/include/Geant4/G4ElectronOccupancy.hh \
+ /usr/local/geant/include/Geant4/G4PrimaryParticle.hh \
+ /usr/local/geant/include/Geant4/G4DynamicParticle.icc \
+ /usr/local/geant/include/Geant4/G4TrackStatus.hh \
+ /usr/local/geant/include/Geant4/G4VUserTrackInformation.hh \
+ /usr/local/geant/include/Geant4/G4Track.icc \
+ /usr/local/geant/include/Geant4/G4HCofThisEvent.hh \
+ /usr/local/geant/include/Geant4/G4VReadOutGeometry.hh \
+ /usr/local/geant/include/Geant4/G4SensitiveVolumeList.hh \
+ /usr/local/geant/include/Geant4/G4TouchableHistory.hh \
+ /usr/local/geant/include/Geant4/G4NavigationHistory.hh \
+ /usr/local/geant/include/Geant4/G4AffineTransform.hh \
+ /usr/local/geant/include/Geant4/G4AffineTransform.icc \
+ /usr/local/geant/include/Geant4/G4NavigationLevel.hh \
+ /usr/local/geant/include/Geant4/G4NavigationLevelRep.hh \
+ /usr/local/geant/include/Geant4/G4NavigationLevelRep.icc \
+ /usr/local/geant/include/Geant4/G4NavigationLevel.icc \
+ /usr/local/geant/include/Geant4/G4EnhancedVecAllocator.hh \
+ /usr/local/geant/include/Geant4/G4NavigationHistory.icc \
+ /usr/local/geant/include/Geant4/G4TouchableHistory.icc \
+ /usr/local/geant/include/Geant4/G4CollectionNameVector.hh \
+ /usr/local/geant/include/Geant4/G4VSDFilter.hh \
+ /usr/local/geant/include/Geant4/G4HCofThisEvent.hh \
+ /usr/local/geant/include/Geant4/G4Step.hh \
+ /usr/local/geant/include/Geant4/G4TouchableHistory.hh \
+ /usr/local/geant/include/Geant4/G4Track.hh \
+ /usr/local/geant/include/Geant4/G4StepPoint.hh include/TrackHit.hh \
+ /usr/local/geant/include/Geant4/G4SDManager.hh \
+ /usr/local/geant/include/Geant4/G4SDStructure.hh \
+ /usr/local/geant/include/Geant4/G4VSensitiveDetector.hh \
+ /usr/local/geant/include/Geant4/G4HCtable.hh
